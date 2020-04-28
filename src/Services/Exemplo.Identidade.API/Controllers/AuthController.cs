f0ff66c67959970d564e81664b6519ddcb7968b1
Thiago Nishio <thiago@aspbrasil.net>
2020-04-27 23:14:06 -0300

    Incluindo básico para acesso à API de Identidade Configurado Migrations para a Identity Alterado propriedades de View Models de Usuário Alterado Controller de Identidade retornando mensagens de erros na resposta Configurado AspNetCore Identity Acrescentei Swagger para documentação básica da API

---
+++ src/Services/Exemplo.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Sample.Identidade.API.ViewModels;
+
+namespace Sample.Identidade.API.Controllers
+{
+    [ApiController]
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager
+            )
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(RegistrarUsuarioViewModel registrarUsuarioVM)
+        {
+            if(!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = registrarUsuarioVM.Email,
+                Email = registrarUsuarioVM.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registrarUsuarioVM.Senha);
+
+            if (result.Succeeded)
+            {
+                return Ok(registrarUsuarioVM);
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(LoginUsuarioViewModel loginUsuarioVM)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(loginUsuarioVM.Email, loginUsuarioVM.Senha,
+                false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok(loginUsuarioVM);
+            }
+
+            if (result.IsLockedOut)
+            {   
+                return BadRequest("Usuário temporariamente bloqueado por tentativas inválidas");
+            }
+
+            return BadRequest("Usuário ou Senha incorretos");
+        }
+    }
+}
---
+++ src/Services/Exemplo.Identidade.API/Startup.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Sample.Identidade.API.Data;
+using System;
+
+namespace Sample.Identidade.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Title = "Exemplo ASP.NET Core Identity WebAPI",
+                Description = "Demonstração de implementação ASP.NET Core Identity REST API, com documentação Swagger e teste de integração.",
+                Contact = new OpenApiContact() { Name = "Thiago Nishio", Email = "thiagonishio@gmail.com" },
+                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
