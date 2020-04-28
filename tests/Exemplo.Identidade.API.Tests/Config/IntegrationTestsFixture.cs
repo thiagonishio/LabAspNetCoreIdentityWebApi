e4816601b2a2c852a81f83b9e0c85bc550a06534
Thiago Nishio <thiago@aspbrasil.net>
2020-04-28 09:25:15 -0300

    Correção build, retirando bilbioteca não utilizada

--- tests/Exemplo.Identidade.API.Tests/Config/IntegrationTestsFixture.cs
+++ tests/Exemplo.Identidade.API.Tests/Config/IntegrationTestsFixture.cs
@@ -1,28 +1,16 @@
-﻿using Bogus;
+﻿using Exemplo.Identidade.API;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Exemplo.Identidade.API;
-using Exemplo.Identidade.API.ViewModels;
 using Xunit;
 
 namespace Exemplo.Identidade.Tests.Config
-{   
+{
     [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
     public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
 
     public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
     {
-        public string AntiForgeryFieldName = "__RequestVerificationToken";
-
-        public string UsuarioEmail;
-        public string UsuarioSenha;
-
-        public string UsuarioToken;
-
         public readonly ExemploAppFactory<TStartup> Factory;
         public HttpClient Client;
 
@@ -38,13 +26,6 @@ namespace Exemplo.Identidade.Tests.Config
 
             Factory = new ExemploAppFactory<TStartup>();
             Client = Factory.CreateClient(clientOptions);
-        }
-
-        public void GerarUserSenha()
-        {
-            var faker = new Faker("pt_BR");
-            UsuarioEmail = faker.Internet.Email().ToLower();
-            UsuarioSenha = faker.Internet.Password(8, false, "", "@1Ab_");
         }
 
         public void Dispose()
